4caa06f56fe0f2f3c31de02ac941da2ff448041e
giovannamoeller <giovannagmoeller@gmail.com>
2021-08-27 13:00:46 -0300

    Initializing repo

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST"]
+  }
+});
+
+const users = [];
+const room = "general";
+
+io.on('connection', socket => {
+  console.log(`Socket conectado: ${socket.id}`);
+
+  socket.on('userActive', user => {
+
+    users.push({
+      id: socket.id,
+      userName: user
+    });
+
+    console.log(users);
+
+    socket.emit('activeUsers', users);
+
+    socket.on('disconnect', () => {
+      console.log(`User ${socket.id} disconnected`);
+    });
+  });
+
+});
+
+server.listen(process.env.PORT || 8080);
